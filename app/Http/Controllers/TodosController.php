9b7a150bcea9bfba09ccbdbad9ba6a4466cf6ffe
Mikael <mikael.srodrigues@gmail.com>
2020-04-03 16:12:57 -0300

    Inserindo comentários de código

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 
 class TodosController extends Controller{
     public function index(){
+        //O método with retorna uma chave que pode ser acessada na view para inclusão de dados, o método all retorna todas as variáveis da classe.
         return view('index')->with('todos', Todo::all());
     }
 
     public function show($todoId){
+        // Retorna o método find que procura todos os dados com base no id passado em parâmetro
         return view('show')->with('todo', Todo::find($todoId));
     }
 
@@ -19,23 +21,30 @@ class TodosController extends Controller{
     }
 
     public function store(){
+        // Retorna uma requisição com todos os dados inseridos nos campos
         $data = request()->all();
+
+        // Nova instância da classe Todo
         $todo = new Todo();
 
+        // Passa as informações obtidas nos campos para a varável $data inserindo-as no vetor
         $todo->name = $data['name'];
         $todo->description = $data['description'];
         $todo->completed = false;
+
+        // Salva as informações inseridas no vetor no banco
         $todo->save();
 
+        // Redireciona para a página principal
         return redirect('/todos');
     }
 
     public function destroy($todoId){
+        // Encontra o dado via get pelo comando find
         $todo = Todo::find($todoId);
-        // dd($todo);
 
+        // Exclui o dado retornado via get com o comando delete
         $todo->delete();
-    
         return redirect('/todos');
     }
 
@@ -46,6 +55,8 @@ class TodosController extends Controller{
     public function update($todoId){
         $data = request()->all();
         $todo = Todo::find($todoId);
+
+        // Atualiza os novos dados dos campos recebidos via post e atualiza com a função update
         $todo->update($data);
         return redirect('/todos');
     }
